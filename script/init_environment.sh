093658948a989f44bedb2d6f38d7296327eea45f
Jensen Hsiao <jensenxiao@gmail.com>
2024-04-07 20:12:17 +0800

    v1.0.3

--- script/init_environment.sh
+++ script/init_environment.sh
@@ -15,23 +15,25 @@ if [[ -n "${SSH_PRIVATE_KEY}" ]]; then
   pacman -S --noconfirm openssh
 fi
 
-# Install asdf-vm
-if [[ ! -f "${WORKING_DIR}/.asdf/asdf.sh" ]]; then
+# Install asdf-vm for tools' version management
+ASDF_HOME=${WORKING_DIR}/.asdf
+
+if [[ ! -f "${ASDF_HOME}/asdf.sh" ]]; then
   git clone https://github.com/asdf-vm/asdf.git \
-      ${WORKING_DIR}/.asdf --branch v0.14.0
+      ${ASDF_HOME} --branch v0.14.0
 fi
 
-# Fix invalid cache to asdf tools' installation
-ln -s ${WORKING_DIR}/.asdf ${HOME}/.asdf
+source ${ASDF_HOME}/asdf.sh
 
-source ${HOME}/.asdf/asdf.sh
+# Fix invalid cache to asdf tools' installation
+ln -s ${ASDF_HOME} ${HOME}/.asdf
 
 # Install ruby environment
 pacman -S --noconfirm libyaml
 
 if ! asdf list ruby ${RUBY_VER} &>/dev/null; then
   # Clean up ruby environments avoiding unnecessary cache
-  rm -rf ${WORKING_DIR}/.asdf/installs/ruby
+  rm -rf ${ASDF_HOME}/installs/ruby
   asdf plugin add ruby
   asdf install ruby ${RUBY_VER}
 fi
@@ -42,4 +44,5 @@ asdf global ruby ${RUBY_VER}
 ruby -v && bundle version
 
 # This is a temporary workaround
+# See https://github.com/actions/checkout/issues/766
 git config --global --add safe.directory "*"
